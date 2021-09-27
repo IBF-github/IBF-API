dbd28a083b140d61f6af2e8e04cf7868a5306ece
angelo scholze <angelo@ibflorestas.org.br>
2021-09-27 18:02:42 -0300

    testes iniciais feitos, td funcionando falta linkar os serviços

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ const APIauvoToken = process.env.APIauvoToken;
 const AuvoBaseURL = "https://api.auvo.com.br/v2/";
 const PipeBaseURL = "https://api.pipedrive.com/v1";
 
+const placeholderId = 5838426;
+const placeholderIdPipe = 94007;
+const placeholderTarefa = "aterramento";
+const placeholderLocal = "curitiba";
+const placeholderOperador = "angelo";
+const placeholderHorario = "meia noite";
+
 app.get("/", (req, res) => {
   res.status(200).send(`
     <html>
@@ -25,20 +32,36 @@ app.get("/", (req, res) => {
     </html>
   `);
 
-  getAuthorization();
+  getClientAuvo(
+    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6WyJhZG1pbmlzdHJhdGl2byBvcGVyYWNpb25hbCIsImFkbWluaXN0cmF0aXZvIG9wZXJhY2lvbmFsIl0sImp0aSI6ImZmYWZiOGQwODNlNDRmNjk5MjY1ZWJjZGM1YjFiNmI0IiwiYXBpS2V5IjoiWUxEUVRqZUEyRWhnWGV4SXpjekhTNGFXS2FZbWVUIiwiYXBpVG9rZW4iOiJZTERRVGplQTJFaHpReVVHYWVqT1NwTXJBdmZkRGdMMSIsIm5iZiI6MTYzMjc3NDI0OCwiZXhwIjoxNjMyNzc2MDQ4LCJpYXQiOjE2MzI3NzQyNDgsImlzcyI6IkF1dm9fQXBpX1B1YmxpY2EiLCJhdWQiOiJVc3VhcmlvX0FwaSJ9.pYBLCCr0oDb8fsC9k_g6lMSQvmS1YScBdwZRWmBQjqcP_fjKKxBGBJrC5e6YB9_TiTTjRmAlKzIoXcKI9U2S0PjgGfPAqrBwsPghQsyOIpzMVtikODQRUxrd1ASZDheumiBfKWeh8gYzpVyd94i5Zxh3Hd9ISAhtUYtYkEqboBVyVpAA-nfKhLDrCUWvMB1c6OBzmbO6gXE8rjyvpkekcBXV0MdO_KX6r7QFfDI2cgLLObt9Ej22hAhUN2kLwsaMTPt5klOvLM-t9ALp6qdxW561sVQYpLN_AuYV1RvdXOGRretxxVfgiedoUGj1xiZCnO7cv4iIi3UIZ59p1-HL3Q",
+    placeholderId
+  );
 });
+
 //adicionar condicional para o app rodar só se a key de terminado for true
-async function appStart() {}
+async function appStart() {
+  const accessToken = await getAuthorization();
+  const auvoCpf = await getClientAuvo(accessToken, placeholderId);
+  const pipeClient = await getClientPipe(auvoCpf);
+  const noteAdded = await addNote(
+    placeholderIdPipe,
+    placeholderTarefa,
+    placeholderLocal,
+    placeholderOperador,
+    placeholderHorario
+  );
+  console.log(noteAdded);
+}
 
 //funçao que gera o token de autorizaçao para o auvo
 async function getAuthorization() {
   const response = await axios(
     `${AuvoBaseURL}/login/?apiKey=${APIauvoKey}&apiToken=${APIauvoToken}`
   );
-  console.log(response);
   const data = await response.data;
-  const acessToken = await data.result.acessToken;
-  return acessToken;
+  const accessToken = await data.result.accessToken;
+  console.log(accessToken);
+  return accessToken;
 }
 
 //funçao que gera o cpf do cliente do auvo para referenciar no pipeDrive
@@ -53,20 +76,20 @@ async function getClientAuvo(acessToken, id) {
     },
     url: `${AuvoBaseURL}/customers/${id}`,
   });
-  console.log(response);
   const data = await response.data;
-  const cpf = await data.result.cpfCnpj;
+  const cpf = await data.result.description;
+  console.log(cpf);
   return cpf;
 }
 
 //funçao para selecionar o cliente no pipeDrive, recebe o cpf do cliente alvo do auvo (kek)
 async function getClientPipe(cpf) {
   const response = await axios(
-    `${PipeBaseURL}/persons/search?fields=custom_fields&api_token=${pipeKey}&term=${cpf}`
+    `${PipeBaseURL}/persons/search?fields=custom_fields&exact_match=true&api_token=${pipeKey}&term=${cpf}`
   );
-  console.log(response);
-  const data = await response.data;
-  const id = await data.items.item.id;
+  const data = await response.data.data;
+  const id = await data.items[0].item.id;
+  console.log(id);
   return id;
 }
 
@@ -86,6 +109,11 @@ async function addNote(id, nomeTarefa, local, operador, horario) {
     url: `${PipeBaseURL}/notes`,
     body: querystring.stringify(body),
   });
-  const success = await response.success;
-  console.log(success);
+  const data = await response.data;
+  const success = await data.success;
+  return success;
 }
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
